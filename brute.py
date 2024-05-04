15f1a6111d41a59aa17325414fe487a81a4ddda2
Gungu <ddoggames7@gmail.com>
2024-05-04 10:33:35 +0200

    QoL stuff with physics

--- brute.py
+++ brute.py
@@ -8,7 +8,7 @@ from pymunk.vec2d import Vec2d
 import sys
 import pymunk.pygame_util
 from pygame._sdl2.video import Window, Texture, Image, Renderer
-from pygame import K_BACKSPACE,K_TAB,K_CLEAR,K_RETURN,K_PAUSE,K_ESCAPE,K_SPACE,K_EXCLAIM,K_QUOTEDBL,K_HASH,K_DOLLAR,K_AMPERSAND,K_QUOTE,K_LEFTPAREN,K_RIGHTPAREN,K_ASTERISK,K_PLUS,K_COMMA,K_MINUS,K_PERIOD,K_SLASH,K_0,K_1,K_2,K_3,K_4,K_5,K_6,K_7,K_8,K_9,K_COLON,K_SEMICOLON,K_LESS,K_EQUALS,K_GREATER,K_QUESTION,K_AT,K_LEFTBRACKET,K_BACKSLASH,K_RIGHTBRACKET,K_CARET,K_UNDERSCORE,K_BACKQUOTE,K_a,K_b,K_c,K_d,K_e,K_f,K_g,K_h,K_i,K_j,K_k,K_l,K_m,K_n,K_o,K_p,K_q,K_r,K_sK_t,K_u,K_v,K_w,K_x,K_y,K_z,K_DELETE,K_KP0,K_KP1,K_KP2,K_KP3,K_KP4,K_KP5,K_KP6,K_KP7,K_KP8,K_KP9,K_KP_PERIOD,K_KP_DIVIDE,K_KP_MULTIPLY,K_KP_MINUS,K_KP_PLUS,K_KP_ENTER,K_KP_EQUALS,K_UP,K_DOWN,K_RIGHT,K_LEFT,K_INSERT,K_HOME,K_END,K_PAGEUP,K_PAGEDOWN,K_F1,K_F2,K_F3,K_F4,K_F5,K_F6,K_F7,K_F8,K_F9,K_F10,K_F11,K_F12,K_F13,K_F14,K_F15,K_NUMLOCK,K_CAPSLOCK,K_SCROLLOCK,K_RSHIFT,K_LSHIFT,K_RCTRL,K_LCTRL,K_RALT,K_LALT,K_RMETA,K_LMETA,K_LSUPER,K_RSUPER,K_MODE,K_HELP,K_PRINT,K_SYSREQ,K_BREAK,K_MENU,K_POWER,K_EURO,K_AC_BACK
+from pygame import K_BACKSPACE,K_TAB,K_CLEAR,K_RETURN,K_PAUSE,K_ESCAPE,K_SPACE,K_EXCLAIM,K_QUOTEDBL,K_HASH,K_DOLLAR,K_AMPERSAND,K_QUOTE,K_LEFTPAREN,K_RIGHTPAREN,K_ASTERISK,K_PLUS,K_COMMA,K_MINUS,K_PERIOD,K_SLASH,K_0,K_1,K_2,K_3,K_4,K_5,K_6,K_7,K_8,K_9,K_COLON,K_SEMICOLON,K_LESS,K_EQUALS,K_GREATER,K_QUESTION,K_AT,K_LEFTBRACKET,K_BACKSLASH,K_RIGHTBRACKET,K_CARET,K_UNDERSCORE,K_BACKQUOTE,K_a,K_b,K_c,K_d,K_e,K_f,K_g,K_h,K_i,K_j,K_k,K_l,K_m,K_n,K_o,K_p,K_q,K_r,K_s, K_t,K_u,K_v,K_w,K_x,K_y,K_z,K_DELETE,K_KP0,K_KP1,K_KP2,K_KP3,K_KP4,K_KP5,K_KP6,K_KP7,K_KP8,K_KP9,K_KP_PERIOD,K_KP_DIVIDE,K_KP_MULTIPLY,K_KP_MINUS,K_KP_PLUS,K_KP_ENTER,K_KP_EQUALS,K_UP,K_DOWN,K_RIGHT,K_LEFT,K_INSERT,K_HOME,K_END,K_PAGEUP,K_PAGEDOWN,K_F1,K_F2,K_F3,K_F4,K_F5,K_F6,K_F7,K_F8,K_F9,K_F10,K_F11,K_F12,K_F13,K_F14,K_F15,K_NUMLOCK,K_CAPSLOCK,K_SCROLLOCK,K_RSHIFT,K_LSHIFT,K_RCTRL,K_LCTRL,K_RALT,K_LALT,K_RMETA,K_LMETA,K_LSUPER,K_RSUPER,K_MODE,K_HELP,K_PRINT,K_SYSREQ,K_BREAK,K_MENU,K_POWER,K_EURO,K_AC_BACK
 
 renderer: Renderer
 window: Window
@@ -27,7 +27,6 @@ Clock = pygame.Clock
 Rect = pygame.Rect
 Sound = pygame.mixer.Sound
 
-
 _version = "1.7.5"
 
 def init(size, title, max_fps=60, fullscreen=False):
@@ -44,7 +43,7 @@ def init(size, title, max_fps=60, fullscreen=False):
     global circle_texture
     pygame.init()
     window = Window(title, size)
-    renderer = Renderer(window)
+    renderer = Renderer(window, accelerated=1)
     _window_surf = window.get_surface()
     if fullscreen:
         window.set_fullscreen()
@@ -277,11 +276,11 @@ class Tilemap:
         return colobs
 
 class PhysicsObject:
-    def __init__(self, image:Texture, pos: Vec2d, rot: int, scale: Vec2d, isTrigger=False, kinematic=False, mass=1, drag=0.005, hasGravity=True, gravity=0.1, friction=1.0, layer='base'):
+    def __init__(self, image:Texture, pos: tuple[int, int], rot: int, scale: tuple[int, int], isTrigger=False, kinematic=False, mass=1, drag=0.005, hasGravity=True, gravity=0.1, friction=1.0, layer='base'):
         self.image = image
-        self.pos = pos
+        self.pos = Vec2d(pos[0], pos[1])
         self.rot = rot
-        self.scale = scale
+        self.scale = Vec2d(scale[0], scale[1])
         self.isTrigger = isTrigger
         self.kinematic = kinematic
         self.drag = drag
@@ -297,9 +296,9 @@ class PhysicsObject:
             body_type = pymunk.Body.KINEMATIC
         else:
             body_type = pymunk.Body.DYNAMIC
-        self.body = pymunk.Body(mass, pymunk.moment_for_box(mass, (scale.x, scale.y)), body_type=body_type)
+        self.body = pymunk.Body(mass, pymunk.moment_for_box(mass, (self.scale.x, self.scale.y)), body_type=body_type)
         self.body.position = pos
-        self.shape = pymunk.Poly.create_box(self.body, size=scale)
+        self.shape = pymunk.Poly.create_box(self.body, size=self.scale)
         self.shape.sensor = isTrigger
         self.shape.friction = friction
         self.shape.elasticity = 0.0
